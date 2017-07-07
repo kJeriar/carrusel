e3a4a0131f9948f02e09d70142475db63a339447
kJeriar <karla.jeria@gmail.com>
2017-07-07 01:19:01 -0400

    flechitas sin movimiento

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,22 @@ $("#boton5").click(function(){
     $("#MyQueen").show();
    
 });
+
+
+function showSlides(n) {
+  var i;
+  var slides = $(".mySlides");
+  var dots = $(".dot");
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+}
